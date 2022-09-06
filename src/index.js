236fc2fbe3a96e9bb3d7f59dbfc57a37f57887af
Alessio Carone <alessio.carone@crealogix.com>
2022-09-06 19:52:03 +0200

    Add formatting and styling a bit typography

--- src/index.js
+++ src/index.js
@@ -1,29 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/main.scss';
-import {ReactComponent as ReactLogo} from './svg/logo.svg';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/main.scss";
+import Icons from "./assets/svg/icons.svg";
+import {ReactComponent as ReactLogo} from "./assets/svg/logo.svg";
+import PropTypes from "prop-types";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const Icon = ({name, color, size}) => (
+	<svg className={`icon icon-${name}`} fill={color} width={size} height={size}>
+		<use xlinkHref={`${Icons}#icon-${name}`} />
+	</svg>
+);
 
 root.render(
-  <React.StrictMode>
-  {
-    <div className="App">
-      <header className="App-header">
-        <ReactLogo />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  }
-  </React.StrictMode>
+	<React.StrictMode>
+		{
+			<main className="main">
+				<header>
+					<h1 class="h1">Design System</h1>
+				</header>
+
+				<br />
+				<br />
+
+				<h1 class="h1">Typography</h1>
+				<h1 class="h2">Typography</h1>
+				<h1 class="h3">Typography</h1>
+				<h1 class="h4">Typography</h1>
+				<h1 class="h5">Typography</h1>
+				<h1 class="h6">Typography</h1>
+
+				<br />
+				<br />
+
+				<Icon name="arrow-down" color="#ff0000" size={35} />
+				<ReactLogo />
+
+				<p>
+					test paragraph size
+					<br />
+					aksdkjasbjkdas
+					<br />
+					jkasdjasdj
+					<br />
+					asdkasd asdasd asd as
+					<br />
+				</p>
+			</main>
+		}
+	</React.StrictMode>
 );
+
+Icon.propTypes = {
+	name: PropTypes.string.isRequired,
+	color: PropTypes.string,
+	size: PropTypes.number,
+};
